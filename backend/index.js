72eca209e03187f8e968f92a57f3c499c2bef7c0
avb24025 <avb24022005@gmail.com>
2025-06-06 21:59:19 +0530

    updated the frontend url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://books-mart.vercel.app/', // Replace with your actual frontend URL
+  credentials: true
+}));
 app.use(express.json());
 
 // Use router correctly
