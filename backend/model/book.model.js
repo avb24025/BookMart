2b57e270c1b9c5c044ce8946f799a1fc4485751f
avb24025 <avb24022005@gmail.com>
2025-06-06 22:50:47 +0530

    updated the book model

--- backend/model/book.model.js
+++ backend/model/book.model.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
 const bookSchema = new mongoose.Schema({
+  id: {
+    type: Number,     // or String if you prefer
+    required: true,
+    unique: true      // enforce uniqueness for your custom id
+  },
   name: {
     type: String,   
     required: true,
